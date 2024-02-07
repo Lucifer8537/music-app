5aa38d583ca8c977fd2b3af10d441dfa7a37ccf7
navodit <navodit50@gmail.com>
2024-02-07 19:52:33 +0530

    updation to music btn functionality

--- src/app/bottom-player/bottom-player.component.ts
+++ src/app/bottom-player/bottom-player.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-bottom-player',
   templateUrl: './bottom-player.component.html',
-  styleUrls: ['./bottom-player.component.css']
+  styleUrls: ['./bottom-player.component.css'],
 })
 export class BottomPlayerComponent {
-
+  playClass = 'fa-solid fa-play';
+  onClickPlay = (audio: HTMLAudioElement) => {
+    if (audio.paused) {
+      audio.play();
+      this.playClass = 'fa-solid fa-pause';
+      // this.startUpdatingCurrentTime();
+    } else {
+      audio.pause();
+      this.playClass = 'fa-solid fa-play';
+    }
+  };
 }
